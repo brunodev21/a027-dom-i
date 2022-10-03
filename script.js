d718518fe46e7af7030fcc504273d55b6aa18613
Bruno Alves de Araujo <88730176+brunodev21@users.noreply.github.com>
2022-10-03 16:28:17 -0300

    primeiro commit

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-const frutas = ["laranja", "limão", "uva"];
+let frutas = ["laranja", "limão", "uva"];
+
+    let laranja = document.getElementById("fruta-1")
+    laranja.innerHTML += frutas[0]
+
+    let limao = document.getElementById("fruta-2")
+    limao.innerHTML += frutas[1]
+
+    let uva = document.getElementById("fruta-3")
+    uva.innerHTML += frutas[2]
+
+let inpuFruta = () => {
+    let fruta = document.getElementById("fruta")
+    console.log(fruta.value += "")
+}
+
+let adicionarFruta = () => {
+    let fruta = document.getElementById("fruta-4")
+    let novaFruta = document.getElementById("fruta")
+    let adicionarNovaFruta = fruta.innerHTML += novaFruta.value 
+    adicionarNovaFruta.value = ""
+}
